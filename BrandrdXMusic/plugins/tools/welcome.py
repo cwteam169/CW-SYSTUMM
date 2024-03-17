3dbc554236e13fcd8c37ee9aa7b18d0af061fc54
cwteam169 <156594586+cwteam169@users.noreply.github.com>
2024-03-17 09:47:22 +0530

    Update welcome.py

--- BrandrdXMusic/plugins/tools/welcome.py
+++ BrandrdXMusic/plugins/tools/welcome.py
@@ -94,7 +94,7 @@ async def greet_group(_, member: ChatMemberUpdated):
 ๏ 𝐌𝐀𝐃𝐄 𝐁𝐘 ➠ @ll_SHIVANG_ll
 ➖➖➖➖➖➖➖➖➖➖➖
 """,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"⦿ ᴀᴅᴅ ᴍᴇ ⦿", url=f"https://t.me/BRANDED_KUDI_BOT?startgroup=true")]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"⦿ ᴀᴅᴅ ᴍᴇ ⦿", url=f"https://t.me/SHIVANG_MUSIC_ROBOT?startgroup=true")]])
         )
     except Exception as e:
         LOGGER.error(e)
