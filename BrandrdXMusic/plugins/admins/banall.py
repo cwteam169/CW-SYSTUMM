2e656db1f1cb8832168da72ac05971bcd21c9f6f
cwteam169 <156594586+cwteam169@users.noreply.github.com>
2024-04-02 12:37:40 +0530

    banall.py

--- BrandrdXMusic/plugins/admins/banall.py
+++ BrandrdXMusic/plugins/admins/banall.py
@@ -2,7 +2,7 @@ from BrandrdXMusic import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from BrandrdXMusic.utils.vip_ban import admin_filter
+from BrandrdXMusic.utils.Brandrd_ban import admin_filter
 from BrandrdXMusic.misc import SUDOERS
 
 BOT_ID = app.me.id  # Corrected this line
